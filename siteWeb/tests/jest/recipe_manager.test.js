35b019e5feb57c1c02e5665f07e6427fe47326bb
Nour <nour-el-houda.lharch@polymtl.ca>
2022-03-08 23:37:28 -0500

    tests for recipe_manager done

--- siteWeb/tests/jest/recipe_manager.test.js
+++ siteWeb/tests/jest/recipe_manager.test.js
@@ -19,20 +19,79 @@ describe("RecipeManager test", () => {
     expect(recipeManager).not.toBeNull();
   });
 
-  it("addRecipe should call saveData with the recipe", () => { });
+  it("addRecipe should call saveData with the recipe", () => {
+    const recipe = {
+      name: 'stub',
+      src: '',
+      time: '10',
+      category: 'keto',
+      ingredients: [
+        {
+          name: "ingredient #1",
+          quantity: "1"
+        },
+        {
+          name: "ingredient #2",
+          quantity: ""
+        }
+      ],
+      tools: [
+        "outil #1",
+        "outil #2"
+      ],
+      steps: [
+        {
+          order: 1,
+          text: "description1",
+          title: "etape1"
+        },
+        {
+          order: 2,
+          text: "description2",
+          title: "etape2"
+        }
+      ],
+      id: 1
+    };
+    const spy = jest.spyOn(storageManager, "saveData");
+    recipeManager.addRecipe(recipe);
+    expect(spy).toHaveBeenCalled();
+   });
 
-  it("getRecipe should return undefined when given an invalid id", () => { });
+  it("getRecipe should return undefined when given an invalid id", () => {
+    // const expected = undefined;
+    const recipe = recipeManager.getRecipe(100);
+    expect(recipe).toBeUndefined();
+   });
 
-  it("getRecipe should return a recipe when given a valid id", () => { });
+  it("getRecipe should return a recipe when given a valid id", () => {
+    const recipe = recipeManager.getRecipe(1);
+    //expect recipe to be the first recipe 
+    expect(recipe).not.toBeUndefined();
+    expect(recipe).toEqual(storageManager.getData().recipes[0]);
+   });
 
   describe("filterRecipes test", () => {
-    it("filterRecipes should return all recipes when no category is specified", () => { });
+    it("filterRecipes should return all recipes when no category is specified", () => {
+      const expected = storageManager.getData().recipes.length;
+      const recipes = recipeManager.filterRecipes('');
+      expect(recipes.length).toEqual(expected);
+     });
 
-    it("filterRecipes should return one recipe when the category is vegetarien", () => { });
+    it("filterRecipes should return one recipe when the category is vegetarien", () => {
+      const recipes = recipeManager.filterRecipes('vegetarien');
+      expect(recipes.length).toEqual(1);
+     });
 
-    it("filterRecipes should return 2 recipe when the category is mediterraneen", () => { });
+    it("filterRecipes should return 2 recipe when the category is mediterraneen", () => {
+      const recipes = recipeManager.filterRecipes('mediterraneen');
+      expect(recipes.length).toEqual(2);
+     });
 
-    it("filterRecipes should return 2 recipe when the category is keto", () => { });
+    it("filterRecipes should return 2 recipe when the category is keto", () => {
+      const recipes = recipeManager.filterRecipes('keto');
+      expect(recipes.length).toEqual(2);
+     });
   });
   describe("filterByIngredient test", () => {
     it("filterByIngredient should return an empty list when searching an invalid ingredient", () => {
